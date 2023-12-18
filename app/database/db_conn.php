0a565ed3ece4cd1c72e92b6785d5e8a6671b42fc
Mehdi <elmahdi20erraji@gmail.com>
2023-12-18 11:17:07 +0100

    db connection + making the db class static

---
+++ app/database/db_conn.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../..');
+$dotenv->load();
+
+class DbConnection {
+    private static $connection;
+
+    private function __construct()
+    {
+        $dbHost = $_ENV['DB_HOST'];
+        $dbUser = $_ENV['DB_USER'];
+        $dbPassword = $_ENV['DB_PASSWORD'];
+        $dbName = $_ENV['DB_NAME'];
+
+        self::$connection = mysqli_connect($dbHost, $dbUser, $dbPassword, $dbName);
+        if (!self::$connection) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+    }
+
+    public static function getConnection() {
+        if (!self::$connection) {
+            new self();
+        }
+        return self::$connection;
+    }
+}
+
+// Usage without instantiating the class
+$connection = DbConnection::getConnection();
+
+if (!$connection) {
+    echo "Db is connected";
+}
